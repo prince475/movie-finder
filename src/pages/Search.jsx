77159e6234f3650af8e3fd7e31f7e170aa252c07
prince475 <danielmwai475@gmail.com>
2025-05-13 14:18:56 +0300

    Creating our search component  that we will use to render our search results.

---
+++ src/pages/Search.jsx
@@ -0,0 +1,11 @@
+import styles from "./Search.module.css";
+
+function Search() {
+  return (
+    <div className={styles.search}>
+      <h1 className={styles.heading}>🔍 Search Results</h1>
+    </div>
+  );
+}
+
+export default Search;
